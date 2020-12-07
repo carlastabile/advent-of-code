81ad49fbf3ad30fa351de967f045b850837c6abb
Carla Urrea Stabile <carlaustabile@gmail.com>
2020-12-07 17:46:22 +0100

    minor refactor

--- day6/day6.rb
+++ day6/day6.rb
@@ -4,23 +4,13 @@
 # \n\n -- group 
 # \n == number of ppl in group 
 # number of letters == number of questions
-require "pry"
-require "awesome_print"
 def load_groups
   File.readlines("input.txt", "\n\n", chomp: true)
 end
 
-def uniq_letters(group)
-  group.gsub("\n", "").chars.uniq
-end
-
-def split_people(group)
-  group.split("\n")
-end
-
-def build_people(groups)
+def people_questions(groups)
   groups.map do |group|
-    people = split_people(group)
+    people = group.split("\n")
 
     {
       people_count: people.length,
@@ -30,10 +20,11 @@ def build_people(groups)
 end
 
 groups = load_groups
+
 # Part 1 
 # Count diff letters per group and sum it up
 result = groups.map do |group|
-  uniq_letters(group)
+  group.gsub("\n", "").chars.uniq
 end
 
 # I love you Ruby 2.7 
@@ -42,11 +33,10 @@ puts result.flatten.tally.values.sum
 
 # Part 2 
 # Count letter that all people in group have
-count = 0
-build_people(groups).each do |answers| 
-  occurences = answers[:letters].tally
-  count += occurences.select{ |_,v| v == answers[:people_count]}.length
-end
-puts count
+result = people_questions(groups).sum do |answers| 
+          occurences = answers[:letters].tally
+          occurences.select{ |_,v| v == answers[:people_count]}.length
+        end
 
+puts result
 
