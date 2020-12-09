3ad505e657e6263a94e530bf80e719234d33783a
Carla Urrea Stabile <carla.urrea@xing.com>
2020-12-09 16:55:15 +0100

    remove each_with_object

--- day7/day7.rb
+++ day7/day7.rb
@@ -5,20 +5,17 @@ def load_bags
   File.readlines("input.txt", chomp: true).each do |line|
     main_bag, inner_bags = line.split(/ bags contain /)
     
-    bags[main_bag] = inner_bags.split(", ").each_with_object([]) do |bag, memo|
+    bags[main_bag] = inner_bags.split(", ").map do |bag|
       
       amount_match = bag.match(/\d/) 
       bag_match = bag.match(/\D+/)[0].split(/ bag/).map(&:lstrip)[0]
 
-      if bag_match == "no other"
-        nil
-      else
-        memo << {
-          amount: amount_match ? amount_match[0].to_i : nil,
-          bag: bag_match
-        }
-      end
-    end
+      {
+        amount: amount_match ? amount_match[0].to_i : nil,
+        bag: bag_match
+      } unless bag_match == "no other"
+    end.compact
   end
   bags
 end
+puts load_bags
