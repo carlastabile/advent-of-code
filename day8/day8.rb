8c4be213ce48ae0c81144dac7ca888ef08d3c4a7
Carla Urrea Stabile <carla.urrea@xing.com>
2020-12-17 15:57:50 +0100

    day8 - remove old code

--- day8/day8.rb
+++ day8/day8.rb
@@ -11,31 +11,6 @@ def instructions
   end
 end
 
-def acc(instructions)
-  acc = 0
-  idx = 0
-
-  while true
-    instruction = instructions[idx]
-    
-    break if instruction[:executed]
-    
-    if instruction[:operation] == :nop
-      instruction[:executed] = true
-      idx += 1 
-      next 
-    end
-
-    acc += instruction[:argument] if instruction[:operation] == :acc
-
-    instruction[:operation] == :jmp ? idx += instruction[:argument] : idx += 1
-
-    instruction[:executed] = true
-  end
-  acc
-end
-
-
 def run_program(instructions)
   acc = 0
   idx = 0
