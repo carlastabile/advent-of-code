6f0f65c83ac46e9f8837ef270dc9520a360c047f
Carla Urrea Stabile <carla.urrea@xing.com>
2020-12-17 15:56:20 +0100

    day8 - part 2

--- day8/day8.rb
+++ day8/day8.rb
@@ -35,5 +35,59 @@ def acc(instructions)
   acc
 end
 
+
+def run_program(instructions)
+  acc = 0
+  idx = 0
+  n = instructions.size 
+  result = {}
+
+  while true
+    instruction = instructions[idx]
+
+    return { acc: acc, idx: idx, terminated: true } if idx >= n
+    return { acc: acc, idx: idx, terminated: false } if instruction[:executed]
+    
+    if instruction[:operation] == :nop
+      instruction[:executed] = true 
+      idx += 1 
+      next 
+    end
+
+    acc += instruction[:argument] if instruction[:operation] == :acc
+
+    instruction[:operation] == :jmp ? idx += instruction[:argument] : idx += 1
+
+    instruction[:executed] = true
+  end
+  { acc: acc, idx: idx, terminated: true }
+end
+
+def generate_combination(instructions, idx)
+  new_instructions = Marshal.load(Marshal.dump(instructions))
+  operation = new_instructions[idx][:operation]
+
+  new_instructions[idx][:operation] = if operation == :jmp 
+                                        :nop
+                                      elsif operation == :nop
+                                        :jmp
+                                      end  
+  new_instructions
+end
+
+def switch_operations_to_terminate(instructions)
+  instructions.each_with_index do |instruction, idx|
+    next unless %i[jmp nop].include? instruction[:operation]
+
+    combination = generate_combination(instructions, idx)
+    accum = run_program(combination)
+    return accum if accum[:terminated] 
+  end
+  accum
+end
+
 # Part 1 
-puts acc(instructions)
+puts run_program(instructions)[:acc]
+
+# Part 2 
+puts switch_operations_to_terminate(instructions)[:acc]
