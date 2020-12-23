cc45684c4ec3678be10d7a90fca4448779e12a64
Carla Urrea Stabile <carlaustabile@gmail.com>
2020-12-23 19:32:51 +0100

    day10 - v1

---
+++ day10/day10.rb
@@ -0,0 +1,29 @@
+# adapter + 1, 2 o 3 
+# pick min difference
+# What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+require "pry"
+
+def adapters
+  File.readlines("input.txt", chomp: true).map(&:to_i).sort
+end
+
+def difference(n)
+  dfs = [1,2,3].map{ |d| n + d }
+  adapters.find{ |m| dfs.include?(m) }
+end
+
+def solution
+  # adapter.max + 3 (max joltage)
+  max = adapters.max + 3
+  result = { 1 => 1, 3 => 1}
+
+  adapters.each do |adapter|
+    break if adapter == max - 3
+    diff = difference(adapter)
+    result[diff - adapter] += 1
+  end
+  puts result
+  result
+end
+
+puts solution.values.reduce(:*)
